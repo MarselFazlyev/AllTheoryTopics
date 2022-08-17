d8df7ff37f10a388c3b3dad84c856c9b152cb56d
Fazlyev Marsel <fazlyev@mail.ru>
2022-08-18 00:27:05 +0300

    update

--- Threads/src/Producer_Consumer/Test.java
+++ Threads/src/Producer_Consumer/Test.java
@@ -5,24 +5,52 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class Test {
-   private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10); //потоеобезопасная очередь
-    public static void main(String[] args) {
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10); //потоеобезопасная очередь
 
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
     }
-    private static void produce() { //заполняем бесконечно очередь
+
+    private static void produce() throws InterruptedException { //заполняем бесконечно очередь
         Random random = new Random();
         while (true) {
-            queue.offer(random.nextInt());
+            queue.put(random.nextInt(100));// существует похожий метод для очереди  offer, но он потоконебезопасный
         }
     }
 
-    private static void consumer(){
+    private static void consumer() throws InterruptedException {
         while (true) {
-            try {
-                queue.take()
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-
+            Random random = new Random();
+            Thread.sleep(100);
+            if (random.nextInt(10) == 5) {
+                System.out.println(queue.take());// потокобезопасный метод для очереди
+                System.out.println("Queue size is " + queue.size());
             }
         }
     }
