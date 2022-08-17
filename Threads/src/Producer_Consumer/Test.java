d87e10e9c80df31f18e17d006d206f196440c097
Fazlyev Marsel <fazlyev@mail.ru>
2022-08-17 22:58:32 +0300

    update

--- Threads/src/Producer_Consumer/Test.java
+++ Threads/src/Producer_Consumer/Test.java
@@ -1,5 +1,15 @@
 package Producer_Consumer;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Test {
+    public static void main(String[] args) {
+        BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+    }
+    private static void produce() {
+        Random random = new Random();
+    }
 
 }
