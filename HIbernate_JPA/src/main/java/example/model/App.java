928d976f606dc9e5385345457efd089b00dfd2e1
Fazlyev Marsel <fazlyev@mail.ru>
2022-09-09 15:48:09 +0300

    module Hibernate updated

--- HIbernate_JPA/src/main/java/example/model/App.java
+++ HIbernate_JPA/src/main/java/example/model/App.java
@@ -6,24 +6,30 @@ import org.hibernate.cfg.Configuration;
 
 public class App {
     public static void main(String[] args) {
+        //пример получения данных класса Person с помощью Hibernate из БД hibernate_demo_db с помоью Hibernate
+
+        //передаем Hibernate класс,являющиймся сущностью.
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        //получаем SessionFactory, чтобы из него получить сессию
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
+            // запускаем транзакцию
             session.beginTransaction();
 
+            //получаем данные из таблицы
             Person person = session.get(Person.class, 1);
-            System.out.println("Hello from Hibernate!!!"+person.getName());
+            System.out.println("Hello from Hibernate!!!" + person.getName());
             System.out.println(person.getAge());
 
+            //завершаем транзакцию
             session.getTransaction().commit();
-        }finally {
+        } finally {
+            //закоываем подключение
             sessionFactory.close();
         }
-
-
-
 
 
     }
--- JDBC/src/main/java/innopolisLection/semipro/DBUtil.java
+++ JDBC/src/main/java/innopolisLection/semipro/DBUtil.java
@@ -32,6 +32,7 @@ public class DBUtil {
                     properties.getProperty("db.password")
             );
 
+
             return connection;
         }
         return connection;
