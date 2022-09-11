dee48bed4970d873c4b923ee06109f7b8e2d0ea2
Fazlyev Marsel <fazlyev@mail.ru>
2022-09-11 20:19:59 +0300

    package kaskaditrovanie added

--- HIbernate_JPA/src/main/java/example/cascadirovanie/Cascadirovanie.java
+++ HIbernate_JPA/src/main/java/example/cascadirovanie/Cascadirovanie.java
@@ -1,20 +1,20 @@
-package example.oneToMany;
+package example.cascadirovanie;
 
-
-import example.oneToMany.model.Item;
-import example.oneToMany.model.Person;
+import example.cascadirovanie.model.Item;
+import example.cascadirovanie.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class OneToMany {
+public class Cascadirovanie {
     public static void main(String[] args) {
-        //пример отнощения "OneToMany"
-        //создаем в БД hibernate_demo_db две таблицы : Person1 (отличаеися от таюлицы Person) и Item
+        //пример каскадирования
 
-        //передаем Hibernate класс,являющиймся сущностью.
+        //передаем Hibernate классы,являющиеся сущностью.
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
 
         //получаем SessionFactory, чтобы из него получить сессию
@@ -24,14 +24,10 @@ public class OneToMany {
         try {
             // запускаем транзакцию
             session.beginTransaction();
-            Person person = session.get(Person.class,3);
-            System.out.println(person);
-            List<Item> items =  person.getItems();
-            System.out.println(items);
-            Item item = new Item(person,"New item from Hibernate");
-            session.persist(item);
-
-
+            Person person = new Person("Test cascading", 34);
+            Item item = new Item(person, "Test cascading item");
+            person.setItems(new ArrayList<>(Collections.singleton(item)));
+            session.persist(person);
 
 
             //завершаем транзакцию
@@ -40,8 +36,6 @@ public class OneToMany {
             //закоываем подключение
             sessionFactory.close();
         }
-
-
 
 
     }
---
+++ HIbernate_JPA/src/main/java/example/cascadirovanie/model/Item.java
@@ -0,0 +1,66 @@
+package example.cascadirovanie.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Item")
+public class Item {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id ;
+
+    @JoinColumn(name = "person_id",referencedColumnName = "id")
+    @ManyToOne
+    private Person owner;
+
+    @Column(name = "item_name")
+    private String item_name;
+
+    public Item(){
+    }
+
+    public Item(Person owner, String item_name) {
+        this.owner = owner;
+        this.item_name = item_name;
+    }
+
+    public Item (String item_name) {
+        this.item_name = item_name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public Person getOwner() {
+
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", owner=" + owner +
+                ", item_name='" + item_name + '\'' +
+                '}';
+    }
+}
